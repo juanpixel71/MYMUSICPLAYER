15b251f34f7fb7442b98f73f2b74dc3635b09f55
juanpixel71 <juanpixel@gmail.com>
2026-09-11 09:24:52 +0200

    Create MainActivity.java

---
+++ android/app/src/main/java/com/mitotalplayer/app/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mitotalplayer.app;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.WindowManager;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Extender la pantalla bajo el Notch/Cámara (Short Edges)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+            layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            getWindow().setAttributes(layoutParams);
+        }
+    }
+}
